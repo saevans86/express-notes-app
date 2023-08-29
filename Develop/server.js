63819b34ecb03dd55261b008b2be56a29b178676
saevans86 <sam_evans1986@hotmail.com>
2023-08-29 12:27:16 -0600

    updated server.js for just writing, still testing why it's not pulling from db.json

--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 app.get('/db', (req, res) =>
@@ -16,56 +17,44 @@ app.get('/db', (req, res) =>
 );
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-
 app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Failed to read notes' });
-    }
-    res.json(JSON.parse(data));
-  });
+  res.json(`${req.body}request recieved to get notes` );
+  console.info(`${req.body} request received to get notes`)
 });
 
 app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
-
-  if (title && text) {
+  console.info(`${req.method} req received to add note`);
+  const { title , note } = req.body;
+  if (title && note) {
     const newNote = {
-      title,
-      text,
+      title, 
+      text, 
       note_id: uuid(),
     };
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Failed to read notes' });
-      }
-
-      const notes = JSON.parse(data);
-      notes.push(newNote);
-
-      fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: 'Failed to save note' });
-        }
-
-        res.status(200).json(newNote);
-      });
-    });
+    const noteString = JSON.stringify(newNote);
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
+    err
+    ? console.error(err)
+    : console.log(`review for ${newNote.title} was written to JSON file`)
+    );
+    const response = {
+      status: 'success',
+      body: newNote 
+    };
+    console.log(response);
+    res.status(201).json(response);
   } else {
-    res.status(400).json('invalid note data');
+    res.status(500).json('error')
   }
-});
+})
+
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes at http://localhost:${PORT}!`)
